b5031ef62c723fef6109f3a8e14e3e297ea94a6d
LaxentaInc <gk559850@gmail.com>
2026-04-29 11:12:47 +0530

    chore: update backups/src/modules/AMA.js

--- backups/src/modules/AMA.js
+++
@@ -1,8 +0,0 @@
-/**
- * Anti Mass Action (AMA) - Legacy Wrapper
- * 
- * This file serves as a wrapper to load the modular AMA system.
- * Logic has been moved to src/modules/helpers/ama/
- */
-
-module.exports = require('./helpers/ama');
